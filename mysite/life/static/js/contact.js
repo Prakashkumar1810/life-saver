e24117fbdfb67c45130b87cb5f014f7e929eb001
Prakashkumar1810 <1810prakashkumar@gmail.com>
2019-03-02 04:46:31 +0530

    Files ready

---
+++ mysite/life/static/js/contact.js
@@ -0,0 +1,12 @@
+document.getElementById('img').addEventListener('click',function(){
+	document.querySelector('.infod').style.display= 'flex';
+});
+document.querySelector('.close').addEventListener('click',function(){
+	document.querySelector('.infod').style.display= 'none';
+})
+document.getElementById('img1').addEventListener('click',function(){
+	document.querySelector('.infop').style.display= 'flex';
+});
+document.querySelector('.close1').addEventListener('click',function(){
+	document.querySelector('.infop').style.display= 'none';
+})
